733b1adcbac5e95b9a6ed6a7f6c125c8178cc043
fattahillah <fattahillahmuhammad48@gmail.com>
2026-04-16 22:52:04 +0700

    Membuat halaman home beranda

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,12 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('home');
+        $page_data = [
+            "page_title"        => 'Layanan JDIH DPRD Kabupaten Batang Hari',
+            "page_description"  => 'Website resmi JDIH DPRD Kabupaten Batang Hari untuk publikasi dokumen hukum daerah.',
+            "page_keywords"     => ['JDIH', 'DPRD Batang Hari', 'Layanan DPRD Batang Hari', 'Informasi Dokumen Hukum'],
+            "page_author"       => 'DPRD Batang Hari',
+        ];
+        return view('home', $page_data);
     }
 }
--- app/Views/home.php
+++ app/Views/home.php
@@ -1,17 +1,5 @@
-<?php
-$db = \Config\Database::connect();
-?>
-<!DOCTYPE html>
-<html lang="en">
+<?= $this->extend('layouts/main.php') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Layanan JDIH DPRD Kabupaten Batang Hari</title>
-</head>
+<?= $this->section('konten') ?>
 
-<body>
-    <h1>Hello, World!</h1>
-</body>
-
-</html>
+<?= $this->endSection() ?>
