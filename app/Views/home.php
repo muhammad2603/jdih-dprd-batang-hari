af7354e3b9683c83d3aca665a3d30a94446a6101
fattahillah <fattahillahmuhammad48@gmail.com>
2026-04-16 21:38:20 +0700

    Membuat halaman home dan test koneksi database

---
+++ app/Views/home.php
@@ -0,0 +1,17 @@
+<?php
+$db = \Config\Database::connect();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layanan JDIH DPRD Kabupaten Batang Hari</title>
+</head>
+
+<body>
+    <h1>Hello, World!</h1>
+</body>
+
+</html>
