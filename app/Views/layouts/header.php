0f3a68d3ff1caaec5ea6da5038eb210721334809
fattahillah <fattahillahmuhammad48@gmail.com>
2026-04-17 18:16:30 +0700

    Membuat komponen header

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -1,3 +1,31 @@
-<header>
-
+<?php
+// __FIX__ Logo atau Identitas kemungkinan juga akan dipakai difooter, query akan dijalankan 2x dalam 1 halaman
+$fe_config = new App\Models\FrontendConfig;
+$get_navigasi_header = $fe_config->getNavigationHeader();
+$get_logo = $fe_config->getLogo();
+$get_identity = $fe_config->getIdentity();
+?>
+<header class="bg-default text-default-foreground">
+    <div class="max-w-7xl mx-auto py-4 px-6 flex justify-between">
+        <a href="<?= base_url() ?>" class="flex gap-3" tabindex="-1">
+            <figure>
+                <img
+                    src="<?= $get_logo["link"] ?>"
+                    alt="<?= $get_logo["content"] ?>"
+                    class="w-12.5 aspect-square rounded-full" />
+            </figure>
+            <div class="header-title">
+                <h1>
+                    <span class="font-semibold block"><?= $get_identity[0]["content"] ?></span>
+                    <span class="text-sm text-muted-foreground"><?= $get_identity[1]["content"] ?></span>
+                </h1>
+            </div>
+        </a>
+        <nav class="header-nav flex gap-8 items-center text-sm">
+            <?php foreach ($get_navigasi_header as $nav): ?>
+                <?php $is_current_page_on_nav = ($nav["content"] === $page_alias ? "text-primary font-semibold" : "transition duration-150 ease-linear hover:text-primary focus:text-primary") ?>
+                <a href="<?= $nav["link"] ?>" class="<?= $is_current_page_on_nav ?> focus:outline-none"><?= $nav["content"] ?></a>
+            <?php endforeach ?>
+        </nav>
+    </div>
 </header>
