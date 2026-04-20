0eaf4154263b948045d9f323798abe055ebde09a
fattahillah <fattahillahmuhammad48@gmail.com>
2026-04-21 03:37:42 +0700

    Membuat tombol unduhan dokumen produk hukum (sedang diselesaikan)

--- app/Controllers/ProdukHukum.php
+++ app/Controllers/ProdukHukum.php
@@ -8,16 +8,18 @@ use App\Models\FrontendConfig;
 
 class ProdukHukum extends BaseController
 {
-    private $frontend_config;
+    private $frontend_config_model;
+    private $produk_hukum_model;
 
     public function __construct()
     {
-        $this->frontend_config = new FrontendConfig;
+        $this->frontend_config_model    = new FrontendConfig;
+        $this->produk_hukum_model       = new \App\Models\ProdukHukum;
     }
 
     public function index()
     {
-        $get_all_data_feconfig = $this->frontend_config->getAllData();
+        $get_all_data_feconfig = $this->frontend_config_model->getAllData();
         $page_keywords = [
             "Produk Hukum Batang Hari",
             "JDIH Batang Hari",
@@ -31,6 +33,7 @@ class ProdukHukum extends BaseController
             "Informasi Hukum Publik",
             "Unduh Peraturan Daerah",
         ];
+        $get_document_produk_hukum_highliht = $this->produk_hukum_model->getProdukHukumHighlight();
         $page_data = [
             "page_title"        => 'Produk Hukum',
             "page_description"  => 'Database lengkap produk hukum daerah Kabupaten Batang Hari yang dapat diakses dan diunduh oleh publik, mencakup peraturan daerah, peraturan bupati, keputusan, dan dokumen hukum lainnya secara transparan dan terstruktur.',
@@ -39,6 +42,7 @@ class ProdukHukum extends BaseController
             "frontend_config"   => array_group_by($get_all_data_feconfig, ["component", "category"]),
             "page_alias"        => 'Produk Hukum',
         ];
+        dd($get_document_produk_hukum_highliht);
         return view("pages/produk_hukum", $page_data);
     }
 }
--- app/Views/pages/produk_hukum.php
+++ app/Views/pages/produk_hukum.php
@@ -82,6 +82,15 @@ $doc_categs = (new App\Models\DocumentCategories)->getDocumentCategories();
                         </svg>
                         <span>Detail</span>
                     </a>
+                    <a href="/path/to/file-document" class="flex items-center gap-2 p-2 hover:bg-muted rounded-lg transition-colors hover:text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-4">
+                            <path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z" />
+                            <path d="M14 2v5a1 1 0 0 0 1 1h5" />
+                            <path d="M12 18v-6" />
+                            <path d="m9 15 3 3 3-3" />
+                        </svg>
+                        <span>PDF</span>
+                    </a>
                 </div>
             </div>
         </div>
