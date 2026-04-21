7645a03cd393ab11808f4e9b1b1dc43a4ca231b8
fattahillah <fattahillahmuhammad48@gmail.com>
2026-04-21 12:39:48 +0700

    Memperbaiki error saat mengambil data dokumen hukum terbaru

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -20,7 +20,7 @@ class Home extends BaseController
     public function index(): string
     {
         $get_all_data_feconfig = $this->frontend_config->getAllData();
-        $get_all_new_document = $this->produk_hukum->getProdukHukumHighlight();
+        $get_all_new_document = $this->produk_hukum->getProdukHukumHighlight()["results"];
         $page_data = [
             "page_title"        => 'Layanan JDIH DPRD Kabupaten Batang Hari',
             "page_description"  => 'Website resmi JDIH DPRD Kabupaten Batang Hari untuk publikasi dokumen hukum daerah.',
@@ -30,9 +30,6 @@ class Home extends BaseController
             "frontend_config"   => array_group_by($get_all_data_feconfig, ["component", "category"]),
             "new_documents"     => $get_all_new_document,
         ];
-
-        // dd($get_all_new_document);
-
         return view('home', $page_data);
     }
 }
