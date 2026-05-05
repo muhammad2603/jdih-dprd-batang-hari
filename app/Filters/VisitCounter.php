e12651edde1c2fb363d00ecdf19393154c0665da
fattahillah <fattahillahmuhammad48@gmail.com>
2026-05-06 00:10:43 +0700

    Menambahkan cookie untuk membataskan update counter dalam 1 hari

--- app/Filters/VisitCounter.php
+++ app/Filters/VisitCounter.php
@@ -6,6 +6,9 @@ use CodeIgniter\Filters\FilterInterface;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use App\Models\Pengunjung;
+use CodeIgniter\I18n\Time;
+
+helper("cookie");
 
 class VisitCounter implements FilterInterface
 {
@@ -13,7 +16,12 @@ class VisitCounter implements FilterInterface
     {
         $pengunjung_model   = new Pengunjung;
         $ip_address_user    = $request->getIPAddress();
+        $timestamp_next_update  = (new Time('+1 day'))->getTimestamp();
+        $set_secure_cookie = $_ENV["CI_ENVIRONMENT"] === "development" ? false : true;
+        $is_cookie_visited_exist = get_cookie("is_visited");
+        if ($is_cookie_visited_exist !== null) return;
         $pengunjung_model->updateCount($ip_address_user);
+        set_cookie("is_visited", $timestamp_next_update, $timestamp_next_update, "localhost", '/', '', $set_secure_cookie, true, 'Strict');
     }
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
