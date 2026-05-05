33b227606cdb0c5fafa1c441dfb9d1447e8a0869
fattahillah <fattahillahmuhammad48@gmail.com>
2026-05-06 00:16:03 +0700

    Perbaikan kecil

--- app/Filters/VisitCounter.php
+++ app/Filters/VisitCounter.php
@@ -17,7 +17,7 @@ class VisitCounter implements FilterInterface
         $pengunjung_model   = new Pengunjung;
         $ip_address_user    = $request->getIPAddress();
         $timestamp_next_update  = (new Time('+1 day'))->getTimestamp();
-        $set_secure_cookie = $_ENV["CI_ENVIRONMENT"] === "development" ? false : true;
+        $set_secure_cookie = getenv("CI_ENVIRONMENT") === "development" ? false : true;
         $is_cookie_visited_exist = get_cookie("is_visited");
         if ($is_cookie_visited_exist !== null) return;
         $pengunjung_model->updateCount($ip_address_user);
