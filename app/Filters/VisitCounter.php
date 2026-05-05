d62678c098a919eba101eb6e8724b83288e5b92a
fattahillah <fattahillahmuhammad48@gmail.com>
2026-05-05 21:31:18 +0700

    Membuat filter untuk update atau insert total pengunjung

--- app/Filters/VisitCounter.php
+++ app/Filters/VisitCounter.php
@@ -5,15 +5,18 @@ namespace App\Filters;
 use CodeIgniter\Filters\FilterInterface;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\Pengunjung;
 
 class VisitCounter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        //
+        $pengunjung_model   = new Pengunjung;
+        $ip_address_user    = $request->getIPAddress();
+        $pengunjung_model->updateCount($ip_address_user);
     }
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
-        //
+        // 
     }
 }
