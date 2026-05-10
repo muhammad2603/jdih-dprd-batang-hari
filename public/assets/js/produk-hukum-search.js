85cd5742b5473048e45092a1b9e46894691559ee
fattahillah <fattahillahmuhammad48@gmail.com>
2026-05-11 02:55:50 +0700

    Mengubah submit pencarian dari AJAX dengan metode tradisional menggunakan query param uri

--- public/assets/js/produk-hukum-search.js
+++ public/assets/js/produk-hukum-search.js
@@ -1,35 +1,17 @@
-const fetchData = (config, success) => {
-    const { url, method, contentType } = config;
-    fetch(url, {
-        method: method,
-        headers: {
-            "Content-Type": contentType
-        },
-        // body: JSON.stringify(payload)
-    })
-        .then(resp => {
-            return resp.json()
-        })
-        .then(data => success(data))
-        .catch(e => console.error(e))
-        .finally(() => console.log('Request ditutup.'));
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     const searchDocumentBtn = document.getElementById("searchDocumentBtn");
     const searchDocumentInput = document.getElementById("searchDocument");
     const selectCategoryDocument = document.getElementById("categoryDocument");
     const selectYearDocument = document.getElementById("yearDocument");
-
     searchDocumentBtn.addEventListener("click", () => {
         const searchDocumentValue = searchDocumentInput.value;
         const categoryDocument = selectCategoryDocument.value;
         const yearDocument = selectYearDocument.value;
-
         const keyword = searchDocumentValue !== '' ? searchDocumentValue.toLowerCase() : false;
         const category = categoryDocument !== '*' ? parseInt(categoryDocument) : false;
         const year = yearDocument !== "*" ? yearDocument : false;
-
+        if (keyword === false && category === false && year === false) return alert("Tentukan kata kunci pencarian, kategori, atau tahun.");
+        if (year !== false && keyword === false && category === false) return alert("Tentukan kata kunci pencarian atau kategori.");
         const queryParams = new URLSearchParams();
         if (keyword !== false) {
             queryParams.append("keyword", keyword)
@@ -40,18 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (year !== false) {
             queryParams.append("year", year)
         }
-        const endpoint = '/api/cari-dokumen?' + queryParams;
-
-        if (keyword === false && category === false && year === false) return alert("Tentukan kata kunci pencarian, kategori, atau tahun.");
-        if (year !== false && keyword === false && category === false) return alert("Tentukan kata kunci pencarian atau kategori.");
-
-        fetchData({
-            url: endpoint,
-            method: 'GET',
-        }, res => {
-            const { data, pagination } = res;
-            document.getElementById('listDokumen').innerHTML = data;
-            document.getElementById('paginationWrapper').innerHTML = pagination.pager;
-        });
+        const endpoint = '/produk-hukum?' + queryParams;
+        window.location.href = endpoint;
     })
 })
