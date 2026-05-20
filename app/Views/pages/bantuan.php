a1914de1df8fbf0375b5c0695c2450cef5806cef
fattahillah <fattahillahmuhammad48@gmail.com>
2026-05-20 22:33:30 +0700

    Membuat halaman responsif diberbagai ukuran perangkat

--- app/Views/pages/bantuan.php
+++ app/Views/pages/bantuan.php
@@ -9,15 +9,15 @@ $faq_link = $frontend_config["Footer"]["Navigasi"][4]["link"];
         <div class="animate">
             <div class="title-wrapper flex gap-3 mb-4">
                 <span class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-10">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-7 md:size-10">
                         <circle cx="12" cy="12" r="10" />
                         <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
                         <path d="M12 17h.01" />
                     </svg>
                 </span>
-                <h1 class="text-4xl font-bold"><?= esc(dot_array_search("jumbotron.title", $pages_meta)) ?></h1>
+                <h1 class="text-2xl md:text-3xl xl:text-4xl font-bold"><?= esc(dot_array_search("jumbotron.title", $pages_meta)) ?></h1>
             </div>
-            <p class="text-lg text-white/80 max-w-2xl"><?= esc(dot_array_search("jumbotron.description", $pages_meta)) ?></p>
+            <p class="text-sm md:text-lg text-white/80 max-w-2xl"><?= esc(dot_array_search("jumbotron.description", $pages_meta)) ?></p>
         </div>
     </div>
 </div>
