9f6f4a30068609dbf03dda86492f1a5c73e5cddc
fattahillah <fattahillahmuhammad48@gmail.com>
2026-05-23 23:22:56 +0700

    Mengubah icon svg inline dengan memanggil icon dari assets/icons.svg

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -59,22 +59,20 @@ foreach ($unique_footer_nav as $group => $items) {
                 <h3 class="font-semibold mb-6 text-center lg:text-start">Kontak</h3>
                 <ul class="space-y-3 flex flex-col text-sm text-white/70">
                     <li class="flex justify-center lg:justify-start items-start gap-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 shrink-0 mt-0.5">
-                            <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
-                            <circle cx="12" cy="10" r="3" />
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 shrink-0 mt-0.5">
+                            <use href="/assets/icons.svg#icon-location"></use>
                         </svg>
                         <span><?= dot_array_search("Lokasi.*.content", $kontak) ?></span>
                     </li>
                     <li class="flex justify-center lg:justify-start items-start gap-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 shrink-0 mt-0.5">
-                            <path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384" />
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 shrink-0 mt-0.5">
+                            <use href="/assets/icons.svg#icon-telephone"></use>
                         </svg>
                         <span><?= dot_array_search("Fax.*.content", $kontak) ?></span>
                     </li>
                     <li class="flex justify-center lg:justify-start items-start gap-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 shrink-0 mt-0.5">
-                            <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
-                            <rect x="2" y="4" width="20" height="16" rx="2" />
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 shrink-0 mt-0.5">
+                            <use href="/assets/icons.svg#icon-mail"></use>
                         </svg>
                         <span><?= dot_array_search("Mail.*.content", $kontak) ?></span>
                     </li>
