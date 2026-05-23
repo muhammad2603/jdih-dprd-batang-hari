1daca2d10ce4dc6885ee1b92742b420df6cff18b
fattahillah <fattahillahmuhammad48@gmail.com>
2026-05-24 00:09:49 +0700

    Mengganti icon inline svg dengan memanggil ID icon difile assets/icons.svg

--- app/Views/pages/faq.php
+++ app/Views/pages/faq.php
@@ -43,8 +43,8 @@
                         <h3 class="font-semibold text-default-foreground"><?= esc($faq["judul"]) ?></h3>
                     </div>
                     <span class="icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 text-muted-foreground shrink-0">
-                            <path d="m6 9 6 6 6-6" />
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 text-muted-foreground shrink-0">
+                            <use href="/assets/icons.svg#icon-caret-down"></use>
                         </svg>
                     </span>
                 </button>
