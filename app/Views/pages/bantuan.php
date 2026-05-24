96b630b27628d728001f1abc54a7674f4cec0afe
fattahillah <fattahillahmuhammad48@gmail.com>
2026-05-25 02:46:54 +0700

    Membuat pop-up notifikasi status pengiriman pesan via email

--- app/Views/pages/bantuan.php
+++ app/Views/pages/bantuan.php
@@ -4,6 +4,26 @@
 $kontak = $frontend_config["Kontak"];
 $faq_link = $frontend_config["Footer"]["Navigasi"][4]["link"];
 ?>
+<div id="toastNotification" class="toast-notification w-1/4 fixed bottom-2 right-2 z-999999999">
+    <div class="notification p-4 flex gap-3 bg-notification text-foreground rounded-lg">
+        <div class="icon self-center shrink">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-7 fill-green-500 stroke-notification">
+                <circle cx="12" cy="12" r="10" />
+                <path d="m9 12 2 2 4-4" />
+            </svg>
+        </div>
+        <div class="notification-message shrink-0 grow">
+            <p class="title font-semibold text-sm"></p>
+            <p class="message mt-0.5 text-sm"></p>
+        </div>
+        <button class="close-notification shrink self-center text-accent-dark-gray cursor-pointer hover:text-accent-light-gray active:text-accent-light-gray focus:outline-none" tabindex="-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-6">
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+            </svg>
+        </button>
+    </div>
+</div>
 <div class="jumbotron bg-primary text-white py-16">
     <div class="max-w-7xl mx-auto px-6">
         <div class="animate">
