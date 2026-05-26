0096552d2968607b55e833aee498905bbdd12ccb
fattahillah <fattahillahmuhammad48@gmail.com>
2026-05-26 15:07:09 +0700

    Refactoring code

--- public/assets/js/bantuan-page.js
+++ public/assets/js/bantuan-page.js
@@ -1,21 +1,41 @@
+function showNotification(el) {
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => el.classList.remove('translate-x-2/4', 'opacity-75', 'pointer-events-none'))
+    })
+}
+function hideNotification(el, delay = true) {
+    if (delay) {
+        setTimeout(() => el.classList.add('pointer-events-none', 'translate-x-2/4', 'opacity-0'), 4000)
+    } else {
+        el.classList.add('pointer-events-none', 'translate-x-2/4', 'opacity-0')
+        setTimeout(() => el.remove(), 200)
+    }
+}
+function removeNotification(el) {
+    hideNotification(el)
+    setTimeout(() => el.remove(), 4100)
+}
+function insertHTML(parentEl, childEl, position = 'afterbegin') {
+    parentEl.insertAdjacentHTML(position, childEl)
+}
+function $(selector) {
+    return document.querySelector(selector)
+}
 let payload = {};
 document.addEventListener("DOMContentLoaded", () => {
-    const btnSendMail = document.getElementById("btnSendMail");
-    const inputEmail = document.getElementById("email");
-    const inputNamaLengkap = document.getElementById("namaLengkap");
-    const inputNomorTelpon = document.getElementById("noTelp");
-    const inputSubjek = document.getElementById("subject");
-    const inputPesan = document.getElementById("message");
-    const toastNotification = document.getElementById("toastNotification");
-    const csrfToken = document.querySelector("input[name=csrf_token]");
-    //! Event listener pada toastNotification dibutuhkan karena notification tidak tersedia sejak awal
+    const btnSendMail = $("#btnSendMail");
+    const inputEmail = $("#email");
+    const inputNamaLengkap = $("#namaLengkap");
+    const inputNomorTelpon = $("#noTelp");
+    const inputSubjek = $("#subject");
+    const inputPesan = $("#message");
+    const toastNotification = $("#toastNotification");
+    const csrfToken = $("input[name=csrf_token]");
     toastNotification.addEventListener('click', e => {
         const closeNotificationBtn = e.target.closest('.close-notification');
-        //! Jika bukan tombol close notification yang diklik, jangan eksekusi perintah selanjutnya
         if (!closeNotificationBtn) return;
-        const getParentNotificationByCloseBtn = closeNotificationBtn.parentElement;
-        getParentNotificationByCloseBtn.classList.add('translate-x-2/4', 'opacity-0', 'pointer-events-none')
-        setTimeout(() => getParentNotificationByCloseBtn.remove(), 200)
+        const notification = closeNotificationBtn.parentElement;
+        hideNotification(notification, false)
     })
     btnSendMail.addEventListener("click", () => {
         payload.userEmail = inputEmail.value.trim();
@@ -23,7 +43,6 @@ document.addEventListener("DOMContentLoaded", () => {
         payload.nomorTelpon = inputNomorTelpon.value.trim();
         payload.subjek = inputSubjek.value.trim();
         payload.pesan = inputPesan.value.trim();
-
         if (
             payload.userEmail === "" ||
             payload.namaLengkap === "" ||
@@ -31,7 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
             payload.subjek === "#" ||
             payload.pesan === ""
         ) return;
-
         fetch('/api/sendmail', {
             method: 'POST',
             headers: {
@@ -51,43 +69,19 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(data => {
                 const { message, notificationId, notification, newToken } = data;
                 csrfToken.value = newToken;
-                toastNotification.insertAdjacentHTML(
-                    'afterbegin',
-                    notification
-                )
+                insertHTML(toastNotification, notification, 'afterbegin')
                 const currentNotificationEl = document.getElementById(notificationId);
-                requestAnimationFrame(() => {
-                    requestAnimationFrame(() => {
-                        currentNotificationEl.classList.remove('translate-x-2/4', 'opacity-75', 'pointer-events-none')
-                    })
-                })
-                setTimeout(() => {
-                    currentNotificationEl.classList.add('pointer-events-none', 'translate-x-2/4', 'opacity-0')
-                }, 4000) // delay untuk menutup notifikasi otomatis
-                setTimeout(() => {
-                    currentNotificationEl.remove()
-                }, 4100) // delay untuk menghapus element notifikasi
+                showNotification(currentNotificationEl, 'translate-x-2/4', 'opacity-75', 'pointer-events-none')
+                removeNotification(currentNotificationEl, 'pointer-events-none', 'translate-x-2/4', 'opacity-0')
             })
             .catch(err => {
                 if (err.code === 403) return alert("Permintaan tidak diizinkan!");
                 const { message, notificationId, notification, newToken } = err;
                 csrfToken.value = newToken;
-                toastNotification.insertAdjacentHTML(
-                    'afterbegin',
-                    notification
-                )
+                insertHTML(toastNotification, notification, 'afterbegin')
                 const currentNotificationEl = document.getElementById(notificationId);
-                requestAnimationFrame(() => {
-                    requestAnimationFrame(() => {
-                        currentNotificationEl.classList.remove('translate-x-2/4', 'opacity-75', 'pointer-events-none')
-                    })
-                })
-                setTimeout(() => {
-                    currentNotificationEl.classList.add('pointer-events-none', 'translate-x-2/4', 'opacity-0')
-                }, 4000) // delay untuk menutup notifikasi otomatis
-                setTimeout(() => {
-                    currentNotificationEl.remove()
-                }, 4100) // delay untuk menghapus element notifikasi
+                showNotification(currentNotificationEl, 'translate-x-2/4', 'opacity-75', 'pointer-events-none')
+                removeNotification(currentNotificationEl, 'pointer-events-none', 'translate-x-2/4', 'opacity-0')
             });
     })
 })
