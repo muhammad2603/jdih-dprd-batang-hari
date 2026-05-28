f742a6d2e5f0916ec6f505445c386878a3bae6c6
fattahillah <fattahillahmuhammad48@gmail.com>
2026-05-29 00:32:31 +0700

    Membuat utility notifikasi

---
+++ public/assets/js/notification.js
@@ -0,0 +1,27 @@
+import { classManipulation } from "./class-manipulation.js";
+function showNotification(el) {
+    classManipulation(el).useAnimFrame(el => el.classList.remove('translate-x-2/4', 'opacity-75', 'pointer-events-none'))
+}
+function hideNotification(el) {
+    classManipulation(el).add('pointer-events-none', 'translate-x-2/4', 'opacity-0')
+}
+function removeNotification(el, delay = 0) {
+    setTimeout(() => $$(el).removeEl(), delay)
+}
+function autoCloseNotification(el) {
+    setTimeout(() => {
+        hideNotification(el)
+        setTimeout(() => removeNotification(el), 100)
+    }, 4000)
+}
+function manualCloseNotification(el) {
+    hideNotification(el)
+    removeNotification(el, 200)
+}
+export {
+    showNotification,
+    hideNotification,
+    removeNotification,
+    autoCloseNotification,
+    manualCloseNotification,
+}
