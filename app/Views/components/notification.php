e43bf99d88c6393bc69460270b94476f177f2989
fattahillah <fattahillahmuhammad48@gmail.com>
2026-06-01 18:01:18 +0700

    Memperbaiki teks pesan notifikasi yang tidak ter-wrap saat pesan memiliki karakter panjang

--- app/Views/components/notification.php
+++ app/Views/components/notification.php
@@ -5,11 +5,11 @@
             <path d="m9 12 2 2 4-4" />
         </svg>
     </div>
-    <div class="notification-message shrink-0 grow">
+    <div class="notification-message shrink grow">
         <p class="title font-semibold text-sm"><?= esc($title) ?></p>
-        <p class="message mt-0.5 text-sm"><?= esc($message) ?></p>
+        <p class="message mt-0.5 text-sm text-wrap"><?= esc($message) ?></p>
     </div>
-    <button class="close-notification shrink self-center text-accent-dark-gray cursor-pointer hover:text-accent-light-gray active:text-accent-light-gray focus:outline-none" tabindex="-1">
+    <button class="close-notification shrink-0 self-center text-accent-dark-gray cursor-pointer hover:text-accent-light-gray active:text-accent-light-gray focus:outline-none" tabindex="-1">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-6">
             <path d="M18 6 6 18" />
             <path d="m6 6 12 12" />
