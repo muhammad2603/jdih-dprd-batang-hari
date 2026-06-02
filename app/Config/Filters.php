c5426c0d92e9d933087378a6df47102618e805c3
fattahillah <fattahillahmuhammad48@gmail.com>
2026-06-02 22:01:49 +0700

    Membuat filter untuk menghapus sesi document access

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -12,6 +12,7 @@ use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\PageCache;
 use CodeIgniter\Filters\PerformanceMetrics;
 use CodeIgniter\Filters\SecureHeaders;
+use App\Filters\PublicConfigFilter;
 
 class Filters extends BaseFilters
 {
@@ -34,6 +35,7 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'publicconfig'  => PublicConfigFilter::class,
     ];
 
     /**
@@ -79,6 +81,12 @@ class Filters extends BaseFilters
         'after' => [
             // 'honeypot',
             // 'secureheaders',
+            'publicconfig' => [
+                "except" => [
+                    'produk-hukum/*',
+                    'document-viewer',
+                ]
+            ]
         ],
     ];
 
