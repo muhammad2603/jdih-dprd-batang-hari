e3157e129fbe948f15f61db47849b40b7e59b315
fattahillah <fattahillahmuhammad48@gmail.com>
2026-06-03 22:07:40 +0700

    Menambahkan link:icon dihead

--- app/Views/pages/document_viewer.php
+++ app/Views/pages/document_viewer.php
@@ -5,6 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
+    <link rel="apple-touch-icon" sizes="57x57" href="/assets/web-icon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/assets/web-icon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/assets/web-icon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/web-icon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/assets/web-icon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/assets/web-icon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/assets/web-icon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/web-icon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/web-icon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/assets/web-icon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/web-icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/assets/web-icon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/web-icon/favicon-16x16.png">
     <link rel="stylesheet" href="/assets/css/base.css">
 </head>
 
