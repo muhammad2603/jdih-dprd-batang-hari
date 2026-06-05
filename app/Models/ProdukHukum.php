26659e22403d282ff89c4349b968627243fe6087
fattahillah <fattahillahmuhammad48@gmail.com>
2026-06-05 20:24:18 +0700

    Menambahkan method pengambilan data dan metadata produk hukum untuk feed

--- app/Models/ProdukHukum.php
+++ app/Models/ProdukHukum.php
@@ -187,7 +187,7 @@ class ProdukHukum extends Model
                     ORDER BY lph.id DESC
                     SEPARATOR ','
                 ) AS berkas",
-                 "mph.abstrak_pdf",
+                "mph.abstrak_pdf",
                 "jumlah_halaman",
                 "ph.created_at",
                 "ph.updated_at"
@@ -408,5 +408,25 @@ class ProdukHukum extends Model
             $builder->where("category_id", $by_category);
         }
         return $builder->findAll();
+    }
+
+    /**
+     * Mengambil produk hukum beserta metadata untuk feed
+     * @param array $fields field yang ingin diambil
+     * @return array
+     */
+    public function getProdukHukumFeed(array $fields): array
+    {
+        return $this
+            ->select($fields)
+            ->join("meta_produk_hukum mph", 'mph.ph_id = ph.id', 'inner')
+            ->join("document_categories doc_categs", 'doc_categs.id = ph.category_id', 'inner')
+            ->join("document_status doc_status", 'doc_status.id = ph.status_id')
+            ->join("sumber_produk_hukum sph", 'sph.id = mph.sumber_id')
+            ->join("klasifikasi_bidang_hukum klf_bh", 'klf_bh.ph_id = ph.id')
+            ->join("kategori_bidang_hukum kbh", 'kbh.id = klf_bh.bidang_hukum_id')
+            ->where("ph.is_publish", true)
+            ->groupBy('ph.id')
+            ->findAll();
     }
 }
