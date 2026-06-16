83ea26e65d3738b0644bd3963fafa1b4320718c9
fattahillah <fattahillahmuhammad48@gmail.com>
2026-06-16 17:47:42 +0700

    Mengubah logika interaksi notifikasi berdasarkan event target yang diklik dan membuat fungsi terpisah untuk membuka atau menutup notifikasi

--- public/assets/js/notification-dashboard.js
+++ public/assets/js/notification-dashboard.js
@@ -1,22 +1,39 @@
+function openNotification(notificationPopUp) {
+    notificationPopUp.classList.remove('hidden')
+}
+function closeNotification(notificationPopUp, dotIconNotification, unreadNotification, dotNotification) {
+    dotIconNotification.remove()
+    notificationPopUp.classList.add('hidden')
+    dotNotification.forEach(el => el.remove())
+    if (!unreadNotification.classList.contains('hidden')) {
+        unreadNotification.remove()
+    }
+}
 document.addEventListener('DOMContentLoaded', () => {
     const notificationBtn = document.getElementById('notificationBtn');
     const notificationPopUp = document.getElementById('notificationPopUp');
     const unreadNotification = document.getElementById('unreadNotification');
     const dotIconNotification = document.getElementById('dotIconNotification');
     let stateNotification = false;
-    notificationBtn.addEventListener('click', function () {
+    window.addEventListener("click", e => {
+        const isNotifBtnClicked = e.target.closest("#notificationBtn");
+        const isNotifPopUpClicked = e.target.closest("#notificationPopUp");
         const dotNotification = notificationPopUp.querySelectorAll('.dot-notification');
+        if (isNotifPopUpClicked) return;
+        if (!isNotifBtnClicked) {
+            if (stateNotification === true) {
+                stateNotification = false;
+                closeNotification(notificationPopUp, dotIconNotification, unreadNotification, dotNotification)
+            }
+            return;
+        }
         if (stateNotification === false) {
             stateNotification = true;
-            notificationPopUp.classList.remove('hidden')
+            openNotification(notificationPopUp)
         } else {
             stateNotification = false;
-            dotIconNotification.remove()
-            notificationPopUp.classList.add('hidden')
-            dotNotification.forEach(el => el.remove())
-            if (!unreadNotification.classList.contains('hidden')) {
-                unreadNotification.remove()
-            }
+            closeNotification(notificationPopUp, dotIconNotification, unreadNotification, dotNotification)
         }
     })
+
 })
