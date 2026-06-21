b020770795228adab5d6eaca13b4a8bb407d2511
fattahillah <fattahillahmuhammad48@gmail.com>
2026-06-22 02:20:55 +0700

    Menggunakan utilitas DOM dan manipulasi atribut class

--- public/assets/js/Form.js
+++ public/assets/js/Form.js
@@ -1,22 +1,26 @@
+import { classManipulation } from "./class-manipulation.js";
+import { $id } from "./dom.js";
 const hiddenTab = tab => {
-    tab.classList.add("hidden")
+    classManipulation(tab).add("hidden");
 }
 const hiddenAllTabs = listTabs => {
     listTabs.forEach(tab => hiddenTab(tab));
 }
 const inactiveButtonTab = button => {
-    button.classList.remove("border-primary", "text-primary")
-    button.classList.add("border-transparent", "text-gray-500", "hover:text-gray-700")
+    classManipulation(button)
+        .remove("border-primary", "text-primary")
+        .add("border-transparent", "text-gray-500", "hover:text-gray-700");
 }
 const inactiveAllButtonsTab = listButtons => {
     listButtons.forEach(btn => inactiveButtonTab(btn))
 }
 const activatedButtonTab = button => {
-    button.classList.remove("border-transparent", "text-gray-500", "hover:text-gray-700")
-    button.classList.add("border-primary", "text-primary")
+    classManipulation(button)
+        .remove("border-transparent", "text-gray-500", "hover:text-gray-700")
+        .add("border-primary", "text-primary");
 }
 const showTab = tab => {
-    tab.classList.remove('hidden')
+    classManipulation(tab).remove('hidden')
 }
 class FormTab {
     constructor(listBtnTab, listTabs) {
@@ -26,7 +30,7 @@ class FormTab {
 
     openTab(currBtn) {
         const tabId = currBtn.dataset.tabId;
-        const targetTabEl = document.getElementById(tabId);
+        const targetTabEl = $id(tabId);
         inactiveAllButtonsTab(this.listBtnTab)
         hiddenAllTabs(this.listTabs)
         activatedButtonTab(currBtn)
