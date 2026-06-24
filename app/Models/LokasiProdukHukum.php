0a83c37587738d022a54f2abe678175ec09661a7
fattahillah <fattahillahmuhammad48@gmail.com>
2026-06-24 20:35:58 +0700

    Membuat method getLocation untuk mengambil lokasi yang tersedia didatabase

---
+++ app/Models/LokasiProdukHukum.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class LokasiProdukHukum extends Model
+{
+    protected $table            = 'lokasi_produk_hukum';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ["id", "lokasi"];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    /**
+     * Ambil lokasi produk hukum yang tersedia didatabase
+     * @return array
+    */
+    public function getLocation(): array
+    {
+        return $this
+            ->select()
+            ->findAll();
+    }
+}
