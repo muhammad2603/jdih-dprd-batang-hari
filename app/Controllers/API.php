bd405c478cdef13a945ecbe20c2ef5946a98cda6
fattahillah <fattahillahmuhammad48@gmail.com>
2026-07-02 02:15:05 +0700

    Menambahkan pagination dan pencarian dokumen berdasarkan kata kunci

--- app/Controllers/API.php
+++ app/Controllers/API.php
@@ -6,6 +6,8 @@ use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 use App\Models\ProdukHukum;
 
+helper("pagination");
+
 class API extends BaseController
 {
     private ProdukHukum $produk_hukum_model;
@@ -23,20 +25,34 @@ class API extends BaseController
                     "message" => "AJAX dibutuhkan untuk mengakses sumber ini."
                 ]);
         }
-        $keyword    = $this->request->getVar('judul') ?? false;
-        $type       = $this->request->getVar('jenis') ?? false;
-        $tahun      = $this->request->getVar('tahun') ?? false;
-        $status     = $this->request->getVar('status') ?? false;
-        $perPage    = 8;
-        $offset     = 0;
-        $produk_hukum_highlight                 = $this->produk_hukum_model->getProdukHukumHighlight($perPage, $offset, $keyword, $type, $tahun, $status);
-        $total_produk_hukum_highlight_found     = $this->produk_hukum_model->getTotalProdukHukumHighlight($keyword, $type, $tahun, $status);
+        $current_page   = $this->request->getVar('page') ?? 1;
+        $keyword        = $this->request->getVar('judul') ?? false;
+        $type           = $this->request->getVar('jenis') ?? false;
+        $tahun          = $this->request->getVar('tahun') ?? false;
+        $status         = $this->request->getVar('status') ?? false;
+        $data_per_page  = 8;
+        $total_produk_hukum_highlight_found = $this->produk_hukum_model->getTotalProdukHukumHighlight($keyword, $type, $tahun, $status);
+        [
+            "page" => $current_page,
+            "offset" => $data_offset,
+            "data_index" => $data_index,
+            "pager" => $mk_pager
+        ] = create_pagination($current_page, $data_per_page, $total_produk_hukum_highlight_found, false, "modern_dynamic");
+        $produk_hukum_highlight = $this->produk_hukum_model->getProdukHukumHighlight($data_per_page, $data_offset, $keyword, $type, $tahun, $status);
         return $this->response
             ->setHeader("Content-Type", 'application/json')
             ->setJSON([
                 "message" => "Data berhasil didapatkan.",
                 "total" => $total_produk_hukum_highlight_found,
-                "view" => view('dashboard/layouts/table_list_produk_hukum', ["produk_hukum" => $produk_hukum_highlight])
+                "data_index" => ($total_produk_hukum_highlight_found !== 0) ? "Menampilkan $data_index dari $total_produk_hukum_highlight_found dokumen yang tersedia" : "",
+                "view" => [
+                    "produk_hukum" => view('dashboard/layouts/table_list_produk_hukum', [
+                        "produk_hukum" => $produk_hukum_highlight,
+                        "page" => $current_page,
+                        "data_offset" => $data_offset
+                    ]),
+                    "pager" => ($total_produk_hukum_highlight_found > $data_per_page) ? $mk_pager : "",
+                ],
             ]);
     }
 }
