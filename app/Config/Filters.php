f5aaa6477a7c893f245afb9133e46fa9e14109fa
fattahillah <fattahillahmuhammad48@gmail.com>
2026-07-01 22:46:04 +0700

    Mengecualikan route login pada filter publicconfig yang mencatat counter pengunjung

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -103,6 +103,7 @@ class Filters extends BaseFilters
                 "except" => [
                     'produk-hukum/*',
                     'document-viewer',
+                    'login'
                 ]
             ]
         ],
