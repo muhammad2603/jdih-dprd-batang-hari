68aaa46caa6f021377819336b8f7897d7b81925b
fattahillah <fattahillahmuhammad48@gmail.com>
2026-07-03 04:20:39 +0700

    Import modul Class Pop Up dan perbaikan pada pop up confirm saat ingin menghapus dokumen dengan menggunakan async & await

--- public/assets/js/manage-documents-page.js
+++ public/assets/js/manage-documents-page.js
@@ -1,57 +1,5 @@
-class PopUp {
-    constructor() {
-        this.wrapper = document.getElementById("popUpWrapper");
-        this.titlePopUp = document.getElementById("titlePopUp");
-        this.warningTextPopUp = document.getElementById("warningTextPopUp");
-        this.messagePopUp = document.getElementById("messagePopUp");
-        this.btnClosePopUp = document.getElementById("closePopUp");
-        this.btnConfirmationPopUp = document.getElementById("confirmationPopUp");
-        this.iconWrapper = document.getElementById("iconWrapper");
-        this.iconPopUp = document.getElementById("icon");
-        this.iconInfo = "icon-information";
-        this.iconEdit = "icon-pencil-square";
-        this.iconDelete = "icon-triangle-alert";
-    }
-    #setIcon(icon, colors) {
-        const { background, foreground } = colors;
-        const getTargetIcon = this[icon];
-        const getUseEl = popUp.iconPopUp.querySelector('use');
-        const getUseHref = getUseEl.href;
-        getUseEl.href.baseVal = `/assets/icons.svg#${getTargetIcon}`;
-        this.iconWrapper.classList.add(background)
-        this.iconPopUp.classList.add(foreground)
-    }
-    #elementConfig(config) {
-        const { title, warning, message, btnConfirmationText, icons } = config;
-        this.#setIcon(icons.type, icons.colors)
-        this.titlePopUp.innerText = title ?? "";
-        this.warningTextPopUp.innerText = warning ?? "";
-        this.messagePopUp.innerText = message ?? "";
-        this.btnConfirmationPopUp.innerText = btnConfirmationText ?? "Konfirmasi";
-    }
-    close() {
-        this.wrapper.classList.remove('flex');
-        this.wrapper.classList.add('hidden');
-    }
-    show() {
-        this.wrapper.classList.remove('hidden');
-        this.wrapper.classList.add('flex');
-    }
-    confirm(config) {
-        this.#elementConfig(config)
-        this.btnClosePopUp.innerText = config.btnCloseText ?? "Batal";
-        this.show()
-        return new Promise((resolve, reject) => {
-            this.btnConfirmationPopUp.onclick = () => {
-                resolve(true)
-            }
-            this.btnClosePopUp.onclick = () => {
-                this.close()
-                reject({ closed: true })
-            }
-        })
-    }
-}
+import { PopUp } from "./Class/PopUp.js";
+
 const useFetch = async (fetchConfig) => {
     const { url, action, headers, success, errors } = fetchConfig;
     try {
@@ -80,43 +28,42 @@ const fetchDocuments = (url, elements) => useFetch({
     }
 });
 const popUp = new PopUp();
-const deleteDocument = (tokenCsrfInput, docId, titleDocument) => {
+const deleteDocument = async (tokenCsrfInput, docId, titleDocument) => {
     const popUpConfig = {
         title: 'Hapus Dokumen',
         warning: 'Dokumen akan dihapus secara permanen dan tidak dapat dipulihkan. Apakah anda yakin?',
         message: titleDocument,
         icons: {
-            type: "iconDelete",
+            type: "ALERT",
             colors: {
                 background: 'bg-red-100',
                 foreground: 'text-red-600'
             }
         }
     };
-    popUp.confirm(popUpConfig)
-        .then(() => {
-            useFetch({
-                url: `/api/hapus-dokumen/${docId}`,
-                action: 'DELETE',
-                headers: {
-                    "X-Requested-With": 'XMLHttpRequest',
-                    "X-CSRF-TOKEN": tokenCsrfInput.value.trim(),
-                },
-                success: resp => {
-                    const { status, message, new_token } = resp;
-                    tokenCsrfInput.value = new_token;
-                    console.log(status ? "OK" : "FAIL")
-                    console.log(message)
-                },
-                errors: err => {
-                    const { message, new_token } = err;
-                    tokenCsrfInput.value = new_token;
-                    document.getElementById("dataIndex").innerText = message;
-                }
-            })
-            popUp.close()
+    const isConfirmed = await popUp.confirm(popUpConfig);
+    if (isConfirmed) {
+        useFetch({
+            url: `/api/hapus-dokumen/${docId}`,
+            action: 'DELETE',
+            headers: {
+                "X-Requested-With": 'XMLHttpRequest',
+                "X-CSRF-TOKEN": tokenCsrfInput.value.trim(),
+            },
+            success: resp => {
+                const { status, message, new_token } = resp;
+                tokenCsrfInput.value = new_token;
+                console.log(status ? "OK" : "FAIL")
+                console.log(message)
+            },
+            errors: err => {
+                const { message, new_token } = err;
+                tokenCsrfInput.value = new_token;
+                document.getElementById("dataIndex").innerText = message;
+            }
         })
-        .catch(err => false)
+        popUp.close()
+    }
 }
 const url = '/api/cari-dokumen?';
 let querySaved = {};
