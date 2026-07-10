e6dd9ba8c0e97032de5ea2ab495554beb17bdec1
fattahillah <fattahillahmuhammad48@gmail.com>
2026-07-11 00:10:24 +0700

    Memperbaiki format desimal dibagian total persentase dari dokumen yang aktif (berlaku) dengan seluruh total data

--- app/Views/dashboard/user/home.php
+++ app/Views/dashboard/user/home.php
@@ -27,7 +27,7 @@ $current_date = Time::now()->toLocalizedString('EEEE, dd MMMM YYYY');
         <div class="card-details flex flex-col gap-1">
             <span class="text-sm text-gray-500">Dokumen Berlaku</span>
             <span class="font-bold text-3xl text-default-foreground"><?= esc($total_document_berlaku) ?></span>
-            <span class="text-xs text-gray-400"><?= esc($percentage_active_document) ?>% dari total dokumen</span>
+            <span class="text-xs text-gray-400"><?= sprintf('%.2f', esc($percentage_active_document)) ?>% dari total dokumen</span>
         </div>
         <div class="card-icon w-11 h-11 bg-green-600 rounded-xl flex items-center justify-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-white">
