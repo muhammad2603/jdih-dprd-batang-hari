1bd85fa6f36a155b2f9442ca4e78e6dddf2e5f41
fattahillah <fattahillahmuhammad48@gmail.com>
2026-07-16 16:55:31 +0700

    Perbaikan kalkulasi dokumen yang tidak bisa dibagi 0 ketika dokumen belum ada masuk ke-record database

--- app/Controllers/UserDashboard.php
+++ app/Controllers/UserDashboard.php
@@ -96,7 +96,7 @@ class UserDashboard extends BaseController
         $curr_month                             = date('m');
         $total_document_current_month           =  $this->produk_hukum_model->getTotalDocumentByMonth($curr_month);
         $total_document_per_category            = $this->produk_hukum_model->getTotalDocumentByCategory();
-        $percentage_of_berlaku_by_total_doc     = ($total_document_berlaku / $total_document) * 100;
+        $percentage_of_berlaku_by_total_doc     = ($total_document > 0) ? ($total_document_berlaku / $total_document) * 100 : 0;
         $meta_data = [
             "title" => 'Dashboard',
             "total_document" => $total_document,
